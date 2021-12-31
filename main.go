84e0fe6d4442c25fcfaef16c1c1164adf3853a20
himanshu <himanshu.tarsoliya@gmail.com>
2021-12-31 10:22:44 +0530

    Setup Database Connection

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/himanshutarsoliya/go-playground/inventoryservice/database"
 	"github.com/himanshutarsoliya/go-playground/inventoryservice/product"
 	"log"
 	"net/http"
@@ -9,6 +11,7 @@ import (
 const basePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(basePath)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
